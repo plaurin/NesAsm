52fe60cf172158ea0f27773b1d910701d3c682a3
Pascal Laurin <pascal.laurin@outlook.com>
2023-12-17 23:26:12 -0500

    Test with SourceGenerator and IncrementalGenerator

---
+++ NesAsm.Example/TestScript.cs
@@ -0,0 +1,15 @@
+﻿using NesAsm.Emulator;
+
+namespace NesAsm.Example;
+
+public class TestScript : ScriptBase
+{
+    public TestScript(NESEmulator emulator) : base(emulator)
+    {
+    }
+
+    public void Start()
+    {
+        LDA(10);
+    }
+}
