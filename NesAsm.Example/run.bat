cea981d9cb185024df2fb9797bd3fadffc278fd5
Pascal Laurin <pascal.laurin@outlook.com>
2023-12-23 23:36:24 -0500

    Add PostBuild Event to compile .nes file and launch Mesen

--- NesAsm.Example/run.bat
+++ NesAsm.Example/run.bat
@@ -1,3 +1,3 @@
 cd output
-"c:\cc65\bin\cl65" --verbose --target nes -o debug.nes SimpleScript.s
-mesen debug.nes
+"c:\cc65\bin\cl65" --verbose --target nes -o example.nes TestScript.s
+mesen example.nes
