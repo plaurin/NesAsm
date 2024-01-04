96c73645d3c9a847afe0893645f54265abe68784
Pascal Laurin <pascal.laurin@outlook.com>
2024-01-03 22:44:18 -0500

    Added Utility class to convert numeric text value

---
+++ NesAsm.Analyzers.Tests/UtilitiesTests.cs
@@ -0,0 +1,35 @@
+﻿namespace NesAsm.Analyzers.Tests;
+
+public class UtilitiesTests
+{
+    [Theory]
+    [InlineData("", "")]
+    [InlineData("0", "0")]
+    [InlineData("123", "123")]
+    [InlineData("65535", "65535")]
+    [InlineData("0x00", "$00")]
+    [InlineData("0x1", "$1")]
+    [InlineData("0xFF", "$FF")]
+    [InlineData("0xA00", "$A00")]
+    [InlineData("0xFFFF", "$FFFF")]
+    [InlineData("0b1", "%1")]
+    [InlineData("0b_101", "%101")]
+    [InlineData("0b_10101010", "%10101010")]
+    [InlineData("0b_1_0_1_0_1_0_1_0", "%10101010")]
+    public void TestParsingNumeric(string input, string expectedOutput)
+    {
+        var result = Utilities.ConvertOperandToNumericText(input);
+        Assert.Equal(expectedOutput, result);
+    }
+
+    [Theory]
+    [InlineData("65536")]
+    [InlineData("-1")]
+    [InlineData("0xA0000")]
+    [InlineData("0b010101010")]
+    public void TestOutOfBoundValueShouldThrow(string invalidInput)
+    {
+        Assert.Throws<InvalidOperationException>(() => Utilities.ConvertOperandToNumericText(invalidInput));
+    }
+
+}
---
+++ NesAsm.Analyzers/Utilities.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System;
+
+namespace NesAsm.Analyzers;
+
+public static class Utilities
+{
+    public static string ConvertOperandToNumericText(string operand)
+    {
+        if (string.IsNullOrWhiteSpace(operand)) return operand;
+
+        if (operand.Length >= 2)
+        {
+            if (operand.Substring(0, 2) == "0x" && byte.TryParse(operand.Substring(2), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out var _)) return $"${operand.Substring(2)}";
+            if (operand.Substring(0, 2) == "0x" && ushort.TryParse(operand.Substring(2), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out var _)) return $"${operand.Substring(2)}";
+
+            if (operand.Substring(0, 2) == "0b")
+            {
+                var result = operand.Substring(2).Replace("_", "");
+                if (result.Length <= 8) return $"%{result}";
+                throw new InvalidOperationException($"Expected parameter of type byte for binary format but found {operand}");
+            }
+        }
+
+        if (byte.TryParse(operand, out var _)) return operand;
+        if (ushort.TryParse(operand, out var _)) return operand;
+
+        throw new InvalidOperationException($"Expected parameter of type byte or ushort but found {operand}");
+    }
+}
