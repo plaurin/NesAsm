8428d3517028a61c244061e252d3a540c9ea60dd
Pascal Laurin <pascal.laurin@outlook.com>
2024-02-07 22:53:25 -0500

    Use const in Controller

--- NesAsm.Example/Controller.cs
+++ NesAsm.Example/Controller.cs
@@ -8,28 +8,43 @@ public class Controller : ScriptBase
     {
     }
 
+    private readonly ushort JOYPAD1 = 0x4016;
+    private readonly ushort JOYPAD2 = 0x4017;
+
+    public readonly byte BUTTON_A = 0b_1000_0000;
+    public readonly byte BUTTON_B = 0b_0100_0000;
+    public readonly byte BUTTON_SELECT = 0b_0010_0000;
+    public readonly byte BUTTON_START = 0b_0001_0000;
+    public readonly byte BUTTON_UP = 0b_0000_1000;
+    public readonly byte BUTTON_DOWN = 0b_0000_0100;
+    public readonly byte BUTTON_LEFT = 0b_0000_0010;
+    public readonly byte BUTTON_RIGHT = 0b_0000_0001;
+
+    public readonly byte Down = 0x20;
+    public readonly byte Pressed = 0x21;
+
     public void ReadControllerOne()
     {
         // Initialize the output memory
         LDAi(1);
-        STA(0x20);
+        STA(Down);
 
         // Send the latch pulse down to the 4021
-        STA(0x4016);
+        STA(JOYPAD1);
         LDAi(0);
-        STA(0x4016);
+        STA(JOYPAD1);
 
         // Read the buttons from the data line
 
         read_loop:
 
-        LDA(0x4016);
+        LDA(JOYPAD1);
         LSR();
-        ROL(0x20);
+        ROL(Down);
         if (BCC()) goto read_loop;
 
         // Temp copy
-        LDA(0x20);
-        STA(0x21);
+        LDA(Down);
+        STA(Pressed);
     }
 }
--- NesAsm.Example/Output/Controller.s
+++ NesAsm.Example/Output/Controller.s
@@ -1,28 +1,40 @@
 ; Auto generated code using the NesAsm project
 .scope Controller
 
+  JOYPAD1 = $4016
+  JOYPAD2 = $4017
+  BUTTON_A = %10000000
+  BUTTON_B = %01000000
+  BUTTON_SELECT = %00100000
+  BUTTON_START = %00010000
+  BUTTON_UP = %00001000
+  BUTTON_DOWN = %00000100
+  BUTTON_LEFT = %00000010
+  BUTTON_RIGHT = %00000001
+  Down = $20
+  Pressed = $21
   .proc readControllerOne
     ; Initialize the output memory
     lda #1
-    sta $20
+    sta Down
 
     ; Send the latch pulse down to the 4021
-    sta $4016
+    sta JOYPAD1
     lda #0
-    sta $4016
+    sta JOYPAD1
 
     ; Read the buttons from the data line
 
   @read_loop:
 
-    lda $4016
+    lda JOYPAD1
     lsr a
-    rol $20
+    rol Down
     bcc @read_loop
 
     ; Temp copy
-    lda $20
-    sta $21
+    lda Down
+    sta Pressed
 
     rts
   .endproc
