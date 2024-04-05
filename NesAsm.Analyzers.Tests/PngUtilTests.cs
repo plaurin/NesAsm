f4b4ac4771d739a6116fa4d0b359eb18585ad37c
Pascal Laurin <pascal.laurin@outlook.com>
2024-04-05 08:38:47 -0400

    Added Png reading utility from https://github.com/EliotJones/BigGustave

---
+++ NesAsm.Analyzers.Tests/PngUtilTests.cs
@@ -0,0 +1,28 @@
+﻿using BigGustave;
+
+namespace NesAsm.Analyzers.Tests;
+
+public class PngUtilTests
+{
+    [Fact]
+    public void TestReadPng()
+    {
+        using (var stream = File.OpenRead(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\TestFiles\ImportChar.png")))
+        {
+            Png image = Png.Open(stream);
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Pixel pixel = image.GetPixel(x, y);
+
+                    if (pixel.R != 0)
+                    {
+                        Console.WriteLine($"({x}, {y}): {pixel}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NesAsm.Analyzers/PngUtil/FilterMethod.cs
@@ -0,0 +1,13 @@
+﻿// Reference : https://github.com/EliotJones/BigGustave
+namespace BigGustave;
+
+/// <summary>
+/// Indicates the pre-processing method applied to the image data before compression.
+/// </summary>
+public enum FilterMethod
+{
+    /// <summary>
+    /// Adaptive filtering with five basic filter types.
+    /// </summary>
+    AdaptiveFiltering = 0
+}
