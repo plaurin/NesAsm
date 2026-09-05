6703238b27a32bb0838ef4784f10d4675a64aa36
Pascal Laurin <pascal.laurin@outlook.com>
2026-09-05 13:37:18 -0400

    RomTreeMap

--- NesAsm.Recompiler/MermaidGenerator.cs
+++ NesAsm.Recompiler/MermaidGenerator.cs
@@ -4,7 +4,7 @@ namespace NesAsm.Recompiler;
 
 public class MermaidGenerator
 {
-    public static void Generate(string outputPath, IEnumerable<Function> functions)
+    public static void GenerateSubRelations(string outputPath, IEnumerable<Subroutine> functions)
     {
         var sb = new StringBuilder();
         sb.AppendLine("```mermaid");
@@ -18,6 +18,28 @@ public class MermaidGenerator
             }
         }
         sb.AppendLine("```");
-        File.WriteAllText(Path.Combine(outputPath, "functions.md"), sb.ToString());
+        File.WriteAllText(Path.Combine(outputPath, "Functions.md"), sb.ToString());
+    }
+
+    public static void GenerateRomTreeMap(string outputPath, IEnumerable<Subroutine> functions, int prgSize)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine("```mermaid");
+        sb.AppendLine("treemap-beta");
+        sb.AppendLine("\"Code\"");
+
+        foreach (var func in functions)
+        {
+            sb.AppendLine($"    \"{func.Address:X4}\" : {func.Size}");
+        }
+
+        sb.AppendLine("\"Data\"");
+
+        sb.AppendLine("\"Unknown\"");
+        var codeSize = functions.Sum(f => f.Size);
+        sb.AppendLine($"    \"Unmapped\" : {prgSize - codeSize}");
+
+        sb.AppendLine("```");
+        File.WriteAllText(Path.Combine(outputPath, "RomTreeMap.md"), sb.ToString());
     }
 }
