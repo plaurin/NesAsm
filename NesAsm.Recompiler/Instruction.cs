a2d3891fcc7600132d59d1397a84a7a1a872e20e
Pascal Laurin <pascal.laurin@outlook.com>
2026-09-06 21:16:34 -0400

    Built in memory labels

--- NesAsm.Recompiler/Instruction.cs
+++ NesAsm.Recompiler/Instruction.cs
@@ -4,6 +4,7 @@ public record Instruction(int Address, byte Opcode, string Mnemonic, int Bytes, 
 {
     public override string ToString()
     {
+
         return Mode switch
         {
             AddressingMode.Implicit => $"${Address:X4}: {Mnemonic}",
@@ -11,13 +12,13 @@ public record Instruction(int Address, byte Opcode, string Mnemonic, int Bytes, 
             AddressingMode.Immediate => $"${Address:X4}: {Mnemonic} #${Argument:X2}",
             AddressingMode.ZeroPage => $"${Address:X4}: {Mnemonic} ${Argument:X2}",
             AddressingMode.ZeroPageX => $"${Address:X4}: {Mnemonic} ${Argument:X2}, X",
-            AddressingMode.Absolute => $"${Address:X4}: {Mnemonic} ${Argument:X4}",
-            AddressingMode.AbsoluteX => $"${Address:X4}: {Mnemonic} ${Argument:X4}, X",
-            AddressingMode.AbsoluteY => $"${Address:X4}: {Mnemonic} ${Argument:X4}, Y",
-            AddressingMode.Relative => $"${Address:X4}: {Mnemonic} ${Argument:X4}",
+            AddressingMode.Absolute => $"${Address:X4}: {Mnemonic} {Labels.GetLabelOrMemoryAddress(Argument)}",
+            AddressingMode.AbsoluteX => $"${Address:X4}: {Mnemonic} {Labels.GetLabelOrMemoryAddress(Argument)}, X",
+            AddressingMode.AbsoluteY => $"${Address:X4}: {Mnemonic} {Labels.GetLabelOrMemoryAddress(Argument)}, Y",
+            AddressingMode.Relative => $"${Address:X4}: {Mnemonic} {Labels.GetLabelOrMemoryAddress(Argument)}",
             AddressingMode.IndirectIndexed => $"${Address:X4}: {Mnemonic} (${Argument:X2}), Y",
-            AddressingMode.Indirect => $"${Address:X4}: {Mnemonic} (${Argument:X4})",
-            _ => $"${Address:X4}: {Mnemonic} {Argument:X4}"
+            AddressingMode.Indirect => $"${Address:X4}: {Mnemonic} ({Labels.GetLabelOrMemoryAddress(Argument)})",
+            _ => $"${Address:X4}: {Mnemonic} {Labels.GetLabelOrMemoryAddress(Argument)}"
         };
     }
 }
---
+++ NesAsm.Recompiler/Labels.cs
@@ -0,0 +1,28 @@
+﻿namespace NesAsm.Recompiler;
+
+public class Labels
+{
+    public static string GetLabelOrMemoryAddress(int? address)
+    {
+        return address switch
+        {
+            0x2000 => "PpuControl_2000",
+            0x2001 => "PpuMask_2001",
+            0x2002 => "PpuStatus_2002",
+            0x2003 => "OamAddr_2003",
+            0x2004 => "OamData_2004",
+            0x2005 => "PpuScroll_2005",
+            0x2006 => "PpuAddr_2006",
+            0x2007 => "PpuData_2007",
+
+            0x4011 => "DmcCounter_4011",
+            0x4012 => "DmcAddress_4012",
+            0x4013 => "DmcLength_4013",
+            0x4014 => "SpriteDma_4014",
+            0x4015 => "ApuStatus_4015",
+            0x4016 => "Controller1_4016",
+            0x4017 => "Ctrl2FrameCounter_4017",
+            _ => $"${address:X4}",
+        };
+    }
+}
